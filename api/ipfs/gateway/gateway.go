36b6218e4ef482b6a3a5d439fbed7891eb37ffbe
Jose M. Tobar <jmtm12804@gmail.com>
2024-04-17 21:56:17 -0600

    added list and delete function

--- api/ipfs/gateway/gateway.go
+++ api/ipfs/gateway/gateway.go
@@ -12,15 +12,15 @@ import (
 )
 
 type Gateway struct {
-	ID        string    `json:"id"`
-	UUID      string    `json:"uuid"`
-	Name      string    `json:"name"`
-	Domain    string    `json:"domain"`
-	Status    string    `json:"status"`
-	IsPrivate bool      `json:"isPrivate"`
-	IsEnabled bool      `json:"isEnabled"`
-	CreatedAT time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
+	ID        interface{} `json:"id"`
+	UUID      string      `json:"uuid"`
+	Name      string      `json:"name"`
+	Domain    string      `json:"domain"`
+	Status    string      `json:"status"`
+	IsPrivate bool        `json:"isPrivate"`
+	IsEnabled bool        `json:"isEnabled"`
+	CreatedAT time.Time   `json:"createdAt"`
+	UpdatedAt time.Time   `json:"updatedAt"`
 }
 
 type GatewayPayload struct {
@@ -80,4 +80,65 @@ func (g *GatewayAPI) CreateGateway(name string, isPrivate, isEnabled bool) (*Gat
 		return nil, err
 	}
 	return &gateway, nil
+}
+
+func (g *GatewayAPI) GetAllGateways() ([]Gateway, error) {
+	g.API.SetBaseURL("https://api.quicknode.com/ipfs/rest/v1/gateway")
+	endpoint := g.API.BaseURL
+
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := g.API.Client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return nil, fmt.Errorf("error: %s", body)
+	}
+
+	var gateways []Gateway
+	err = json.Unmarshal(body, &gateways)
+	if err != nil {
+		return nil, err
+	}
+	return gateways, nil
+}
+
+func (g *GatewayAPI) DeleteGatewayByName(gatewayName string) error {
+	g.API.SetBaseURL("https://api.quicknode.com/ipfs/rest/v1/gateway")
+	endpoint := fmt.Sprintf("%s/%s", g.API.BaseURL, gatewayName)
+
+	req, err := http.NewRequest("DELETE", endpoint, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := g.API.Client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return fmt.Errorf("error: %s", body)
+	}
+
+	return nil
 }
--- api/ipfs/gateway/gateway_test.go
+++ api/ipfs/gateway/gateway_test.go
@@ -1,10 +1,18 @@
 package gateway
 
 import (
+	"fmt"
+	"math/rand"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/jmtx1020/go_quicknode/client"
+)
+
+const (
+	charset = "0123456789"
+	length  = 6
 )
 
 func TestCreateGateway(t *testing.T) {
@@ -13,12 +21,58 @@ func TestCreateGateway(t *testing.T) {
 	apiWrapper := client.NewAPIWrapper(apiToken, "https://api.quicknode.com/ipfs/rest/v1/gateway")
 	gatewayAPI := &GatewayAPI{API: apiWrapper}
 
+	randomStr := randomString(length)
+
 	_, err := gatewayAPI.CreateGateway(
-		"testing-api-gateway-4",
+		fmt.Sprintf("testing-api-%s", randomStr),
 		true,
 		false,
 	)
 	if err != nil {
 		t.Errorf("Unexpected error: %v", err)
 	}
+}
+
+func TestGetAllGateways(t *testing.T) {
+	apiToken := os.Getenv("QUICKNODE_API_TOKEN")
+
+	apiWrapper := client.NewAPIWrapper(apiToken, "https://api.quicknode.com/ipfs/rest/v1/gateway")
+	gatewayAPI := &GatewayAPI{API: apiWrapper}
+
+	_, err := gatewayAPI.GetAllGateways()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+}
+
+func TestDeleteGateway(t *testing.T) {
+	apiToken := os.Getenv("QUICKNODE_API_TOKEN")
+
+	apiWrapper := client.NewAPIWrapper(apiToken, "https://api.quicknode.com/ipfs/rest/v1/gateway")
+	gatewayAPI := &GatewayAPI{API: apiWrapper}
+
+	gateways, err := gatewayAPI.GetAllGateways()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	testing_gateway := ""
+	for _, gateway := range gateways {
+		if strings.Contains(gateway.Name, "testing-api-") {
+			testing_gateway = gateway.Name
+		}
+	}
+
+	err = gatewayAPI.DeleteGatewayByName(testing_gateway)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+}
+
+func randomString(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
 }
